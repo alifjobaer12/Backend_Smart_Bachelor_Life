6ebb7b9ba0030a4692cd50534838d0007fb5a79c
Pranta <prantakumerpandit@gmail.com>
2026-04-04 13:51:05 +0600

    Read in expense route

--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -16,3 +16,25 @@ const {
 
 //  Create (manager)
 router.post("/", authManagerMiddleware, createExpense);
+const express = require("express");
+const router = express.Router();
+
+const {
+  createExpense,
+  getExpenses,
+  updateExpense,
+  deleteExpense,
+  getExpenseSummary,
+} = require("../controllers/expense.controller");
+
+const {
+  authUserMiddleware,
+  authManagerMiddleware,
+} = require("../middlewares/auth.middleware");
+
+//  Create (manager)
+router.post("/", authManagerMiddleware, createExpense);
+
+//  Read (all users)
+router.get("/", authUserMiddleware, getExpenses);
+
