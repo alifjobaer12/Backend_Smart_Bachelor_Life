5e23b63c3ec7becc72e36b9489d82e96aa2bd4c0
Pranta <prantakumerpandit@gmail.com>
2026-04-04 13:26:38 +0600

    post bazar entry bazar route

---
+++ src/routes/bazar.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createBazar,
+  getBazar,
+  updateBazar,
+  deleteBazar,
+  getBazarSummary,
+} = require("../controllers/bazar.controller");
+
+const {
+  authUserMiddleware,
+  authManagerMiddleware,
+} = require("../middlewares/auth.middleware");
+
+//  Create (manager)
+router.post("/", authManagerMiddleware, createBazar);
