7841ee3bf291cf773ee91b06051d8d5d561dca8e
Pranta <prantakumerpandit@gmail.com>
2026-04-04 13:01:43 +0600

    get menu in menu controller

--- src/controllers/menu.controller.js
+++ src/controllers/menu.controller.js
@@ -28,3 +28,26 @@ exports.createMenu = async (req, res) => {
     });
   }
 };
+
+// Get Menus
+exports.getMenus = async (req, res) => {
+  try {
+    const { groupID, date } = req.query;
+
+    const query = { groupID };
+    if (date) query.date = date;
+
+    const menus = await Menu.find(query).populate("userID", "displayName email");
+
+    res.status(200).json({
+      success: true,
+      data: menus,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch menus",
+      error: error.message,
+    });
+  }
+};
