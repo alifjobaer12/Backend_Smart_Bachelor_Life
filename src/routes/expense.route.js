5d9e724c01d139162d1beb77425216234b8562ab
Pranta <prantakumerpandit@gmail.com>
2026-04-04 13:50:19 +0600

    create in expense route

---
+++ src/routes/expense.route.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createExpense,
+  getExpenses,
+  updateExpense,
+  deleteExpense,
+  getExpenseSummary,
+} = require("../controllers/expense.controller");
+
+const {
+  authUserMiddleware,
+  authManagerMiddleware,
+} = require("../middlewares/auth.middleware");
+
+//  Create (manager)
+router.post("/", authManagerMiddleware, createExpense);
