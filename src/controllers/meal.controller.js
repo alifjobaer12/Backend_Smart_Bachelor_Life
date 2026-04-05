22f4a5bd47e040f3c54d593e37adb0b5a4ff63b6
Pranta <prantakumerpandit@gmail.com>
2026-04-06 00:32:02 +0600

    fix: meal controller validation

--- src/controllers/meal.controller.js
+++ src/controllers/meal.controller.js
@@ -5,27 +5,34 @@ const { logger, getLogContext, getErrorMeta } = require("../utils/logger.util");
 // Create Meal
 exports.createMeal = async (req, res) => {
   const logCtx = getLogContext(req);
+  const { groupID, date, mealCount } = req.body;
+
+  logger.info("Create meal attempt", {
+    ...logCtx,
+    groupID,
+    date,
+    mealCount,
+  });
+
   try {
-    const user = await User.findOne({ firebaseUid: req.user.uid });
+    const user = await User.findOne({ firebaseUid: req.user.firebaseUid });
 
     if (!user) {
       logger.warn("User not found in createMeal", logCtx);
       return res.status(404).json({ success: false, message: "User not found" });
     }
 
-    const { groupID, date, mealCount } = req.body;
-
     const meal = await Meal.create({
       userID: user._id,
       groupID,
-      date,
+      date: date || new Date(),
       mealCount,
     });
 
-    logger.info("Meal created", { ...logCtx, mealId: meal._id });
+    logger.info("Meal created", { ...logCtx, mealId: meal._id, userId: user._id });
     res.status(201).json({ success: true, data: meal });
   } catch (error) {
-    logger.error("Error creating meal", { ...logCtx, error: getErrorMeta(error) });
+    logger.error("Error creating meal", { ...logCtx, groupID, date, mealCount, error: getErrorMeta(error) });
     res.status(500).json({ success: false, error: error.message });
   }
 };
@@ -33,18 +40,25 @@ exports.createMeal = async (req, res) => {
 // Get Meals
 exports.getMeals = async (req, res) => {
   const logCtx = getLogContext(req);
+  const { groupID, date } = req.query;
+
+  logger.info("Get meals attempt", {
+    ...logCtx,
+    groupID,
+    date,
+  });
+
   try {
-    const { groupID, date } = req.query;
 
     const query = { groupID };
     if (date) query.date = date;
 
     const meals = await Meal.find(query).populate("userID", "displayName email");
 
-    logger.info("Meals fetched", logCtx);
+    logger.info("Meals fetched", { ...logCtx, count: meals.length });
     res.status(200).json({ success: true, data: meals });
   } catch (error) {
-    logger.error("Error fetching meals", { ...logCtx, error: getErrorMeta(error) });
+    logger.error("Error fetching meals", { ...logCtx, groupID, date, error: getErrorMeta(error) });
     res.status(500).json({ success: false, error: error.message });
   }
 };
@@ -52,6 +66,13 @@ exports.getMeals = async (req, res) => {
 // Update
 exports.updateMeal = async (req, res) => {
   const logCtx = getLogContext(req);
+
+  logger.info("Update meal attempt", {
+    ...logCtx,
+    mealId: req.params.id,
+    updates: req.body,
+  });
+
   try {
     const meal = await Meal.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
@@ -63,7 +84,7 @@ exports.updateMeal = async (req, res) => {
     logger.info("Meal updated", { ...logCtx, mealId: meal._id });
     res.json({ success: true, data: meal });
   } catch (error) {
-    logger.error("Error updating meal", { ...logCtx, error: getErrorMeta(error) });
+    logger.error("Error updating meal", { ...logCtx, mealId: req.params.id, updates: req.body, error: getErrorMeta(error) });
     res.status(500).json({ success: false, error: error.message });
   }
 };
@@ -71,6 +92,12 @@ exports.updateMeal = async (req, res) => {
 // Delete
 exports.deleteMeal = async (req, res) => {
   const logCtx = getLogContext(req);
+
+  logger.info("Delete meal attempt", {
+    ...logCtx,
+    mealId: req.params.id,
+  });
+
   try {
     const meal = await Meal.findByIdAndDelete(req.params.id);
 
@@ -82,7 +109,8 @@ exports.deleteMeal = async (req, res) => {
     logger.info("Meal deleted", { ...logCtx, mealId: meal._id });
     res.json({ success: true });
   } catch (error) {
-    logger.error("Error deleting meal", { ...logCtx, error: getErrorMeta(error) });
+    logger.error("Error deleting meal", { ...logCtx, mealId: req.params.id, error: getErrorMeta(error) });
     res.status(500).json({ success: false, error: error.message });
   }
 };
+
