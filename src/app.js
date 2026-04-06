360263661777c4fc7573bf5bbc0e1d3ca28613d0
alifjobaer12 <alifjobaer12@gmail.com>
2026-04-07 00:02:38 +0600

    add frontent dev port

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ const app = express();
 const allowedOrigins = [
 	"http://localhost:3000",
 	"http://localhost:3001",
-	"http://localhost:5000",
+	"http://localhost:5173",
 ];
 
 // CORS configuration & middleware
