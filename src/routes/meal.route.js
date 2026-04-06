5aa49fd6229aa80f5812941ed4048c5619a2e4db
alifjobaer12 <alifjobaer12@gmail.com>
2026-04-06 21:30:59 +0600

    add cache

--- src/routes/meal.route.js
+++ src/routes/meal.route.js
@@ -2,41 +2,61 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  createMeal,
-  getMeals,
-  updateMeal,
-  deleteMeal,
- // getMealSummary,
+	createMeal,
+	getMeals,
+	updateMeal,
+	deleteMeal,
 } = require("../controllers/meal.controller");
 
 const { authUserMiddleware } = require("../middlewares/auth.middleware");
+const cacheMiddleware = require("../middlewares/cache.middleware");
 
 /**
  * - create a new meal
  * - POST /api/meals
  * - protected route, requires valid Firebase ID token
  */
-router.post("/", authUserMiddleware, createMeal);
+router.post(
+	"/",
+	authUserMiddleware,
+	cacheMiddleware.invalidateCache(["meal"]),
+	createMeal,
+);
 
 /**
  * - get meals
  * - GET /api/meals
  * - protected route, requires valid Firebase ID token
  */
-router.get("/", authUserMiddleware, getMeals);
+router.get(
+	"/",
+	authUserMiddleware,
+	cacheMiddleware.getFromCache("meal", 120),
+	getMeals,
+);
 
 /**
  * - update a meal
  * - PATCH /api/meals/:id
  * - protected route, requires valid Firebase ID token
  */
-router.patch("/:id", authUserMiddleware, updateMeal);
+router.patch(
+	"/:id",
+	authUserMiddleware,
+	cacheMiddleware.invalidateCache(["meal"]),
+	updateMeal,
+);
 
 /**
  * - delete a meal
  * - DELETE /api/meals/:id
  * - protected route, requires valid Firebase ID token
  */
-router.delete("/:id", authUserMiddleware, deleteMeal);
+router.delete(
+	"/:id",
+	authUserMiddleware,
+	cacheMiddleware.invalidateCache(["meal"]),
+	deleteMeal,
+);
 
 module.exports = router;
